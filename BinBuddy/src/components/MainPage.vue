3e9d068b101e0a202af055caa38cebb66d791a94
seobkun325 <seobkun325@gmail.com>
2025-01-23 14:19:22 +0900

    feat : change background image -> forest.svg

---
+++ BinBuddy/src/components/MainPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <h2>This is main page</h2>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.container {
+  background-image: url("img/forest-bg.svg");
+  width: 100vw;
+  height: 100vh;
+  background-size: 100%;
+  background-repeat: no-repeat;
+  background-position: center;
+}
+</style>
