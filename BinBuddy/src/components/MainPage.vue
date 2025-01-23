0c90bbb4b598e4e6c2c055c0dcaac49a1db84914
seobkun325 <seobkun325@gmail.com>
2025-01-23 15:05:05 +0900

    feat : create footer in main-page. left : logo & title, right : mypage, login button

--- BinBuddy/src/components/MainPage.vue
+++ BinBuddy/src/components/MainPage.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="container">
     <div class="header">
-      <div></div>
+      <div class="header-left"><img :src="logo" /> BinBuddy</div>
+      <div class="header-right">
+        <div>My Page</div>
+        <div class="space"></div>
+        <div>Login</div>
+      </div>
     </div>
     <div class="content">
       <h1>let me see your trash...</h1>
@@ -12,22 +17,43 @@
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import logo from "../assets/img/logo.svg";
+</script>
 
 <style scoped>
 .container {
-  background-image: url("img/forest-bg.svg");
+  background-image: url("../assets/img/forest-bg.svg");
   width: 100vw;
   height: 100vh;
   background-size: cover;
   background-repeat: no-repeat;
   background-position: center;
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   justify-content: space-between;
 }
 .header {
   height: 100px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.header-left {
+  font-size: 40px;
+  margin-left: 80px;
+  font-weight: bold;
+}
+.header-right {
+  font-size: 20px;
+  margin-right: 100px;
+  display: flex;
+  justify-content: space-between;
+  font-weight: bold;
+}
+.header-right-inner {
+  display: flex;
+  align-items: space-between;
 }
 .content {
 }
@@ -36,5 +62,9 @@
 }
 .footer {
   height: 100px;
+}
+
+.space {
+  width: 40px;
 }
 </style>
